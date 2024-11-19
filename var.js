5097c7390ee77856759938b710cf371b2b168bb8
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-11-19 22:08:23 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,8 @@
+// var.js
+const config = {
+    site: "https://surveyheart.com/form/673ca8dc2b3f0a03759754cc"
+};
+// var.js
+const config2 = {
+site2: "https://surveyheart.com/form/673b2be22b3f0a03758566a8" // Replace with the actual URL
+};

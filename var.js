fd59c0b2feffc5a8f9ae73179dd87fbc930d4883
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-11-22 19:06:51 +0530

    Update var.js

--- var.js
+++ var.js
@@ -6,3 +6,7 @@ const config = {
 const config2 = {
 site2: "https://surveyheart.com/form/673b2be22b3f0a03758566a8" // Replace with the actual URL
 };
+
+
+// scince start-   https://surveyheart.com/form/674087eeebad23283684a25a
+//stop scince -https://surveyheart.com/form/674087eeebad23283684a25a

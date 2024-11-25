4bab51dbb6a8e02dd341ffa18bbf2db09c66a9fa
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-11-25 13:34:57 +0530

    Update var.js

--- var.js
+++ var.js
@@ -1,12 +1,11 @@
 // var.js
 const config = {
-    site: "https://surveyheart.com/form/673ca8dc2b3f0a03759754cc"
+    site: "https://surveyheart.com/form/674087eeebad23283684a25a"
 };
 // var.js
 const config2 = {
-site2: "https://surveyheart.com/form/673b2be22b3f0a03758566a8" // Replace with the actual URL
+site2: "https://surveyheart.com/form/6740885debad23283684a9a8" // Replace with the actual URL
 };
 
 
-// scince start-   https://surveyheart.com/form/674087eeebad23283684a25a
-//stop scince -https://surveyheart.com/form/674087eeebad23283684a25a
+

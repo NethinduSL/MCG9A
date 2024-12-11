2a5147700938c709a15aa0f127e181f82692712f
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-11 20:09:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const schedule = {
     "2024-12-04": "Today is free",
     "2024-12-05": "History 2",
         "2024-12-06": "Science 2",
-    "2024-12-10": "Mathematics 2"
+    "2024-12-10": "Mathematics 2",
+        "2024-12-12": "Buddhism 2"
 };
 
 function getTime() {

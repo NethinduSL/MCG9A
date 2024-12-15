2bf284a6ad9988d360d85b3fc64b376d102cbe76
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-15 22:03:12 +0530

    Update var.js

--- var.js
+++ var.js
@@ -1,6 +1,6 @@
 // var.js
 const config = {
-    site: "https://surveyheart.com/form/675b0cc7a9ae150403e96099"
+    site: "https://surveyheart.com/form/675f049f88657618197fc0cd"
 };
 // var.js
 const config2 = {

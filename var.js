53c2d0f208f5ef7371692208e047dbaba9dac38b
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-18 21:37:20 +0530

    Update var.js

--- var.js
+++ var.js
@@ -1,10 +1,10 @@
 // var.js
 const config = {
-    site: "https://surveyheart.com/form/676047b9f544d217b4ea8647"
+    site: "https://surveyheart.com/form/6762f2ff2505a317f8f29532"
 };
 // var.js
 const config2 = {
-site2: "https://surveyheart.com/form/676047d52505a317f8d4bba8" // Replace with the actual URL
+site2: "https://surveyheart.com/form/6762f32eac71e817d525b851" // Replace with the actual URL
 };
 
 
